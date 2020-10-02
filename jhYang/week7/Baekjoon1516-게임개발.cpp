15e88690e871bf7683cfd381d613f9b7028e0009
yangjae33 <wogurrhtn@gmail.com>
2020-10-02 19:08:45 +0900

    week7

--- jhYang/week7/Baekjoon1516-게임개발.cpp
+++ jhYang/week7/Baekjoon1516-게임개발.cpp
@@ -3,33 +3,23 @@
 
 using namespace std;
 
-int btime[501];
-int ptime[501]={0,};
-//pair<int,pair<int,int> > : arrnum / nodeidx / buildtime
-vector<pair<int,int> > seq;
-vector<vector<int> > arr;
-vector<vector<int> > tree;
-priority_queue<pair<int,int> ,vector<pair<int,int> >, greater<pair<int,int> > > pq;
-int visited[501] = {0,};
+int btime[501] = {0,}; // build time
+int ptime[501] = {0,}; // 누적 build time
+pair<int,int> seq[501]; // inDegree, index
+vector<int> tree[501]; // 그래프
+queue<int> que; // seq( < inDegree, index > )
 
-int bfs(){
-    while(!pq.empty()){
-        pair<int,int> idx = pq.top();
-        pq.pop();
-        
-        if(visited[idx.second]==1){
-            continue;
-        }
-        //printf("%d ",idx.second);
-        visited[idx.second] = 1;
-        for(int i = 0; i<tree[idx.second].size(); i++){
-            int k = tree[idx.second][i];
-            if(visited[k]==0){
-                seq[k].first-=1;
-                if(seq[k].first == 0)
-                    pq.push(seq[k]);
-                ptime[seq[k].second] = max(ptime[seq[k].second],btime[seq[k].second]+ptime[idx.second]);
-            }
+void bfs(){
+    while(!que.empty()){
+        int idx = que.front();
+        que.pop();
+        for(int i = 0; i<tree[idx].size(); i++){
+            int k = tree[idx][i];
+
+            seq[k].first-=1;
+            if(seq[k].first == 0)
+                que.push(k);
+            ptime[k] = max(ptime[k],ptime[idx]+btime[idx]);
         }
     }
 }
@@ -37,30 +27,27 @@ int main(){
     cin.tie(0);
     ios_base::sync_with_stdio(false);
     int n;cin>>n;
-    arr.resize(n+1);
-    seq.resize(n+1);
-    tree.resize(n+1);
     for(int i = 1; i<n+1; i++){
         int input;
+        int cnt = 0;
         cin>>input;
         btime[i] = input;
-        ptime[i] = input;
         while(true){
             cin>>input;
             if(input == -1) 
                 break;
-            arr[i].push_back(input);
+            cnt++;
             tree[input].push_back(i);
         }
-        seq[i].first = arr[i].size();
+        seq[i].first = cnt;
         seq[i].second = i;
-        if(arr[i].size() == 0){
-            pq.push(seq[i]);
+        if(cnt == 0){
+            que.push(i);
         }
     }
     //BFS
     bfs();
     for(int i = 1; i<=n; i++){
-        printf("%d\n",ptime[i]);
+        printf("%d\n",btime[i]+ptime[i]);
     }
 }
